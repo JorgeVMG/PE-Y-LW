73618ce3911bc370f2e0e40050be7ff6012bf585
JorgeVMG <musicag230204@gmail.com>
2024-06-09 23:12:03 -0300

    tp 11

--- Trabajo Practico 11/Act5A.js
+++ Trabajo Practico 11/Act5A.js
@@ -1,38 +1,52 @@
-var n1,n2;
-n1 = prompt("valor x", 0);
-n2 = prompt("valor x", 0);
-function sumar(n1,n2){
+function sumar(){
+    var n1,n2;
+    n1 = prompt("valor 1", 0);
+    n2 = prompt("valor 2", 0);
     var suma,salida;
     suma=parseInt(n1)+parseInt(n2);
     salida=document.getElementById("salida");
     salida.value+=n1+'+'+n2+'='+suma+'\n';
 }
-function resta(n1,n2){
+function resta(){
     var resta,salida;
+    var n1,n2;
+    n1 = prompt("valor 1", 0);
+    n2 = prompt("valor 2", 0);
     resta=parseInt(n1)-parseInt(n2);
     salida=document.getElementById("salida");
     salida.value+=n1+'-'+n2+'='+resta+'\n';
 }
-function division(n1,n2){
+function division(){
     var division,salida;
+    var n1,n2;
+    n1 = prompt("valor 1", 0);
+    n2 = prompt("valor 2", 0);
     division=parseInt(n1)-parseInt(n2);
     salida=document.getElementById("salida");
     salida.value+=n1+'-'+n2+'='+division+'\n';
 }
-function multiplicacion(n1,n2){
+function multiplicacion(){
+    var n1,n2;
+    n1 = prompt("valor 1", 0);
+    n2 = prompt("valor 2", 0);
     var multiplicacion,salida;
     multiplicacion=parseInt(n1)*parseInt(n2);
     salida=document.getElementById("salida");
     salida.value+=n1+'x'+n2+'='+multiplicacion+'\n';
 }
-function potencia(n1,n2){
+function potencia(){
     var potencia,salida;
+    var n1,n2;
+    n1 = prompt("valor 1", 0);
+    n2 = prompt("valor 2", 0);
     potencia= Math.pow(n1, n2);
     salida=document.getElementById("salida");
-    salida.value+=n1+'potencia '+n2+'='+potencia+'\n';
+    salida.value+=n1+' potencia '+n2+' = '+potencia+'\n';
 }
-function cuadrado(n1){
+function cuadrado(){
     var cuadrado,salida;
+    var n1;
+    n1 = prompt("valor 1", 0);
     cuadrado=Math.sqrt(n1)
     salida=document.getElementById("salida");
     salida.value+=n1+'+'+n2+'='+cuadrado+'\n';
